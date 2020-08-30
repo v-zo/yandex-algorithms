3bbe1f7a353473c5db31543a731343b9f3e9c502
18237432 <vpzorin@sberbank.ru>
2020-08-30 15:47:35 +0300

    optomised

--- 1.intro/F_three_chips/encode.js
+++ 1.intro/F_three_chips/encode.js
@@ -1,4 +1,8 @@
-fun = (data, N, indC) => {
+const getCache = (data, ind1, ind2, arcmN) => Math.abs(data[ind1] + data[ind2] + arcmN);
+
+const fun = (data, N, indC) => {
+    let ind1, ind2;
+
     if (indC === 0) {
         ind1 = data[1] > data[2] ? 1 : 2;
         ind2 = data[1] > data[2] ? 2 : 1;
@@ -10,52 +14,55 @@ fun = (data, N, indC) => {
         ind2 = data[0] > data[1] ? 1 : 0;
     }
 
+    const arcmN = data[indC] - N;
+    let cache = getCache(data, ind1, ind2, arcmN);
+
     data.forEach((el, i, arr) => {
         if (indC === i) {
             return;
         }
 
-        if (
-            Math.abs(el + arr[ind2] + arr[indC] - N)
-            < Math.abs(arr[ind1] + arr[ind2] + arr[indC] - N)
+        const elSum = el + arcmN;
+
+        if (ind2 !== i &&
+            (Math.abs(elSum + arr[ind2]) < cache)
         ) {
-            if (ind2 === i) {
-                return;
-            }
             ind1 = i;
+            cache = getCache(arr, ind1, ind2, arcmN)
             return;
         }
 
-        if (
-            Math.abs(el + arr[ind1] + arr[indC] - N)
-            < Math.abs(arr[ind1] + arr[ind2] + arr[indC] - N)
+        if (ind1 !== i &&
+            (Math.abs(elSum + arr[ind1]) < cache)
         ) {
-            if (ind1 === i) {
-                return;
-            }
             ind2 = i;
+            cache = getCache(arr, ind1, ind2, arcmN)
             return;
         }
     });
 
-    return [data[ind1], data[ind2], data[indC]];
+    return data[ind1] + data[ind2] + data[indC];
 }
 
-rec = (data, N) => {
-    res = fun(data, N, 0);
-    clos = Math.abs((res[0] + res[1] + res[2]) - N);
+const rec = (data, N) => {
+    let res = fun(data, N, 0);
+    let clos = Math.abs(res - N);
 
     for (i = 1; i < data.length; i++) {
-        cc = fun(data, N, i);
-        sum = Math.abs((cc[0] + cc[1] + cc[2]) - N);
+        const curr = fun(data, N, i);
+        const sum = Math.abs(curr - N);
+
+        if (curr === N) {
+            return curr.toString();
+        }
 
         if (sum < clos) {
             clos = sum;
-            res = cc;
+            res = curr;
         }
     }
 
-    return (res[0] + res[1] + res[2]).toString();
+    return res.toString();
 }
 
 const encode = (lines) => {
