b495c463901dcd805eb8a6c01c565aae8d312f20
18237432 <vpzorin@sberbank.ru>
2020-12-02 23:24:17 +0300

    tests OK

--- sprint-1/final-B/appB.go
+++ sprint-1/final-B/appB.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -11,56 +13,67 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanWords)
-
-	scanner.Scan()
-	//n, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan() // read useless n
 
 	writer := bufio.NewWriter(os.Stdout)
 
-	writeHome := func(home string) {
-		writer.WriteString(home)
+	writeStringWithSpace := func(str string) {
+		writer.WriteString(str)
 		writer.WriteString(" ")
 	}
 
-	writeBodySegment := func(homeCounter int) {
-		for i := 0; i < homeCounter; i++ {
-			writeHome("*")
+	writeHeadSegment := func(homeCounter int) {
+		for i := homeCounter; i > 0; i-- {
+			writeStringWithSpace(strconv.Itoa(i))
 		}
 	}
 
-	writeHeadSegment := func(homeCounter int) {
-		for i := 0; i < homeCounter; i++ {
-			writeHome("x")
+	writeBodySegment := func(homeCounter int) {
+		halfOfSegment := homeCounter / 2
+		for i := 1; i <= halfOfSegment+homeCounter%2; i++ {
+			writeStringWithSpace(strconv.Itoa(i))
+		}
+		for i := halfOfSegment; i > 0; i-- {
+			writeStringWithSpace(strconv.Itoa(i))
 		}
 	}
 
 	writeTailSegment := func(homeCounter int) {
-		for i := 0; i < homeCounter; i++ {
-			writer.WriteString("*")
-			if i < homeCounter-1 {
+		for i := 1; i <= homeCounter; i++ {
+			writer.WriteString(strconv.Itoa(i))
+			if i < homeCounter {
 				writer.WriteString(" ")
 			}
 		}
 	}
 
 	homeCounter := 0
-	isHead := true
 	for scanner.Scan() {
 		home := scanner.Text()
 
 		if home == "0" {
 			if homeCounter > 0 {
-				if isHead {
-					writeHeadSegment(homeCounter)
-				} else {
-					writeBodySegment(homeCounter)
-				}
+				writeHeadSegment(homeCounter)
+			}
 
-				isHead = false
+			writeStringWithSpace(home)
+			break
+		} else {
+			homeCounter++
+		}
+	}
+
+	homeCounter = 0
+	for scanner.Scan() {
+		home := scanner.Text()
+
+		if home == "0" {
+			if homeCounter > 0 {
+				writeBodySegment(homeCounter)
 				homeCounter = 0
 			}
 
-			writeHome(home)
+			writeStringWithSpace(home)
 		} else {
 			homeCounter++
 		}
@@ -70,11 +83,10 @@ func main() {
 		writeTailSegment(homeCounter)
 	}
 
-	//writer.WriteString("\n")
+	writer.WriteString("\n")
 	writer.Flush()
 
-	//fmt.Println("\n")
-	//fmt.Println("2 1 0 1 2 1 0 1 0 0 1 0 0 1 0 1 2 1 0 1")
+	fmt.Println("0 0 1 0 0 0 0 1 0 0 1 2 1 0 1 0 1 0 0 0") // TODO: remove
 }
 
 func openFile(path string) *os.File {
