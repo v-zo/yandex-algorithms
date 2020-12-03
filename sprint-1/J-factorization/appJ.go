64b5f0b88193f552c826b9bd02c944b1b867d30f
18237432 <vpzorin@sberbank.ru>
2020-12-03 09:26:42 +0300

    add J

--- sprint-1/J-factorization/appJ.go
+++ sprint-1/J-factorization/appJ.go
@@ -2,71 +2,33 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strconv"
 )
-
-type Array []int
-type PrimeFactors []int
 
 func main() {
 	file := openFile("input.txt")
 	n := readData(file)
 	file.Close()
 
-	var pf PrimeFactors
-	pf = pf.FindFactors(n)
+	writer := bufio.NewWriter(os.Stdout)
 
-	fmt.Println(pf)
+	FindFactors(n, writer)
+
+	writer.WriteString("\n")
+	writer.Flush()
 }
 
-func (pf PrimeFactors) FindFactors(n int) PrimeFactors {
+func FindFactors(n int, writer *bufio.Writer) {
 	for i := 2; i <= n; i++ {
 		if n%i == 0 {
-			pf = append(pf, i)
-			return pf.FindFactors(n / i)
+			writer.WriteString(strconv.Itoa(i))
+			writer.WriteString(" ")
+			FindFactors(n/i, writer)
+
+			return
 		}
 	}
-
-	return pf
-}
-
-func GetPrimes(n int) []int {
-	var numbers Array = []int{0, 0}
-	for i := 2; i <= n; i++ {
-		numbers = append(numbers, i)
-	}
-
-	for num := 2; num <= n; num++ {
-		if numbers[num] > 0 {
-			for j := 2 * num; j <= n; j += num {
-				numbers[j] = 0
-			}
-		}
-	}
-
-	return numbers.Filter(testNonZero)
-}
-
-func testNonZero(item int) bool {
-	if item > 0 {
-		return true
-	}
-
-	return false
-}
-
-func (arr Array) Filter(test func(int) bool) []int {
-	var filteredArr []int
-
-	for _, item := range arr {
-		if test(item) {
-			filteredArr = append(filteredArr, item)
-		}
-	}
-
-	return filteredArr
 }
 
 func readData(inputFile *os.File) int {
