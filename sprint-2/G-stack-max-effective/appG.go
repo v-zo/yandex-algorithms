2dba0500513d57a10c81156e8d38837635151b31
18237432 <vpzorin@sberbank.ru>
2020-12-19 13:04:45 +0300

    appG

--- sprint-2/G-stack-max-effective/appG.go
+++ sprint-2/G-stack-max-effective/appG.go
@@ -11,11 +11,7 @@ import (
 
 type Stack struct {
 	data []int
-}
-
-type EffectiveStack struct {
-	stack Stack
-	max   Stack
+	max  int
 }
 
 func main() {
@@ -26,21 +22,29 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 	scanner.Scan()
 
-	startStack := Stack{[]int{}}
-	maxStack := Stack{[]int{math.MinInt64}}
-	es := &EffectiveStack{startStack, maxStack}
+	stack := createStack()
 
 	for scanner.Scan() {
-		executeCommand(es, scanner.Text())
+		executeCommand(stack, scanner.Text())
 	}
 }
 
-func (stack *Stack) peek() int {
+func createStack() *Stack {
+	return &Stack{[]int{}, math.MinInt64}
+}
+
+func (stack *Stack) top() int {
 	return stack.data[len(stack.data)-1]
 }
 
 func (stack *Stack) push(x int) {
-	stack.data = append(stack.data, x)
+	val := x
+	if x >= stack.max {
+		val = 2*x - stack.max
+		stack.max = x
+	}
+
+	stack.data = append(stack.data, val)
 }
 
 func (stack *Stack) pop() string {
@@ -48,50 +52,40 @@ func (stack *Stack) pop() string {
 		return "error"
 	}
 
+	val := stack.top()
+	if val >= stack.max {
+		stack.max = 2*stack.max - val
+	}
+
 	stack.data = stack.data[:len(stack.data)-1]
 	return ""
 }
 
-func (es *EffectiveStack) push(x int) {
-	es.stack.data = append(es.stack.data, x)
-
-	if x > es.max.peek() {
-		es.max.push(x)
+func (stack *Stack) peek() int {
+	val := stack.top()
+	if val > stack.max {
+		return stack.max
 	}
+
+	return val
 }
 
-func (es *EffectiveStack) pop() string {
-	if len(es.stack.data) == 0 {
-		return "error"
-	}
-
-	lastEl := es.stack.peek()
-	max := es.max.peek()
-
-	if lastEl == max {
-		es.max.pop()
-	}
-
-	es.stack.data = es.stack.data[:len(es.stack.data)-1]
-	return ""
-}
-
-func (es *EffectiveStack) getMax() string {
-	if len(es.stack.data) == 0 {
+func (stack *Stack) getMax() string {
+	if len(stack.data) == 0 {
 		return "None"
 	}
 
-	return strconv.Itoa(es.max.peek())
+	return strconv.Itoa(stack.max)
 }
 
-func executeCommand(es *EffectiveStack, line string) {
+func executeCommand(stack *Stack, line string) {
 	if line == "get_max" {
-		fmt.Println(es.getMax())
+		fmt.Println(stack.getMax())
 	}
 
 	if line == "pop" {
-		error := es.pop()
-		if error == "error" {
+		err := stack.pop()
+		if err == "error" {
 			fmt.Println("error")
 		}
 	}
@@ -99,7 +93,7 @@ func executeCommand(es *EffectiveStack, line string) {
 	res := strings.Split(line, " ")
 	if res[0] == "push" {
 		num, _ := strconv.Atoi(res[1])
-		es.push(num)
+		stack.push(num)
 	}
 }
 
