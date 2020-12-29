946dc97ab58650e3ba226def85ebcd58ebe1a6ab
18237432 <vpzorin@sberbank.ru>
2020-12-30 00:37:03 +0300

    finalB

--- sprint-2/final-B-calculator/appB.go
+++ sprint-2/final-B-calculator/appB.go
@@ -14,6 +14,7 @@ package main
 import (
 	"bufio"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -27,36 +28,8 @@ func main() {
 	Solve(reader, writer)
 }
 
-func Solve(reader *bufio.Reader, writer *bufio.Writer) {
-	scanner := bufio.NewScanner(reader)
-	scanner.Split(bufio.ScanWords)
-	scanner.Scan()
-
-	ch := scanner.Text()
-	stack := &Stack{&Node{ch, nil}}
-
-	for scanner.Scan() {
-		//writer.WriteString(scanner.Text())
-		ch = scanner.Text()
-
-		isOperator := strings.Contains("+-*/", ch)
-
-		if isOperator {
-
-		} else {
-			stack.push(ch)
-		}
-
-		//writer.WriteString(stack.head.value)
-	}
-
-	//writer.WriteString("\n")
-
-	writer.Flush()
-}
-
 type Node struct {
-	value string
+	value int
 	prev  *Node
 }
 
@@ -64,14 +37,92 @@ type Stack struct {
 	head *Node
 }
 
-func (st *Stack) push(val string) {
-	st.head = &Node{val, st.head}
+type Calculator struct {
+	stack *Stack
 }
 
-func (st *Stack) pop() string {
-	st.head = &Node{st.head.value, st.head.prev}
+func Solve(reader *bufio.Reader, writer *bufio.Writer) {
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
 
-	return st.head.value
+	ch := scanner.Text()
+	calculator := createCalculator(ch)
+
+	for scanner.Scan() {
+		ch = scanner.Text()
+		calculator.process(ch)
+	}
+
+	writer.WriteString(strconv.Itoa(calculator.getResult()))
+	writer.WriteString("\n")
+	writer.Flush()
+}
+
+func createCalculator(initialChar string) *Calculator {
+	num, _ := strconv.Atoi(initialChar)
+	stack := &Stack{&Node{num, nil}}
+
+	return &Calculator{stack}
+}
+
+func (clr *Calculator) getResult() int {
+	return clr.stack.head.value
+}
+
+func (clr *Calculator) process(ch string) {
+	isOperator := strings.Contains("+-*/", ch)
+
+	if isOperator {
+		switch ch {
+		case "+":
+			a := clr.stack.pop()
+			b := clr.stack.pop()
+			clr.stack.push(a + b)
+		case "-":
+			a := clr.stack.pop()
+			b := clr.stack.pop()
+			clr.stack.push(b - a)
+		case "*":
+			a := clr.stack.pop()
+			b := clr.stack.pop()
+			clr.stack.push(a * b)
+		case "/":
+			a := clr.stack.pop()
+			b := clr.stack.pop()
+			clr.stack.push(dev(b, a))
+		}
+	} else {
+		num, _ := strconv.Atoi(ch)
+		clr.stack.push(num)
+	}
+}
+
+func dev(a int, b int) int {
+	abs := func(i int) int {
+		if i < 0 {
+			return -i
+		}
+
+		return i
+	}
+
+	if abs(a) < abs(b) && a < 0 && b > 0 {
+		return a
+	} else {
+		return a / b
+	}
+}
+
+func (stack *Stack) push(val int) {
+	stack.head = &Node{val, stack.head}
+}
+
+func (stack *Stack) pop() (value int) {
+	value = stack.head.value
+	stack.head = stack.head.prev
+
+	return
 }
 
 func openFile(path string) *os.File {
