4dd6942235a4fa1b2d9f37d83e942bd43732b544
18237432 <vpzorin@sberbank.ru>
2021-01-02 18:14:16 +0300

    appL

--- sprint-3/L-two-bycycles/appL.go
+++ sprint-3/L-two-bycycles/appL.go
@@ -23,14 +23,8 @@ func Solve(reader *bufio.Reader, writer *bufio.Writer) {
 	line3, _ := reader.ReadString('\n')
 
 	days, _ := strconv.Atoi(strings.Trim(line1, "\n"))
-	amountStrings := strings.Split(strings.Trim(line2, "\n"), " ")
+	amounts := strings.Split(strings.Trim(line2, "\n"), " ")
 	bikeCost, _ := strconv.Atoi(line3)
-
-	var amounts []int
-	for _, str := range amountStrings {
-		a, _ := strconv.Atoi(str)
-		amounts = append(amounts, a)
-	}
 
 	res1, res2 := findTwoDays(amounts, days, bikeCost)
 
@@ -50,79 +44,75 @@ func conv(i int) string {
 	return strconv.Itoa(i + 1)
 }
 
-func findTwoDays(amounts []int, days int, bikeCost int) (res1 int, res2 int) {
+func findTwoDays(amounts []string, days int, bikeCost int) (res1 int, res2 int) {
 	res2 = -1
 	res1 = -1
 
 	if days == 1 {
-		if amounts[0] >= bikeCost {
+		a, _ := strconv.Atoi(amounts[0])
+		if a >= bikeCost {
 			res1 = 0
 		}
-		if amounts[0] >= 2*bikeCost {
+		if a >= 2*bikeCost {
 			res2 = 0
 		}
 
 		return
 	}
 
+	last, _ := strconv.Atoi(amounts[days-1])
+	if last < bikeCost {
+		return
+	}
+
 	res1 = findDay(amounts, 0, days-1, bikeCost)
 
-	if res1 > -1 {
-		res2 = findDay(amounts, res1+1, days-1, 2*bikeCost)
+	if last >= 2*bikeCost {
+		res2 = findDay(amounts, res1, days-1, 2*bikeCost)
 	}
 
 	return
 }
 
-func findDay(amounts []int, start int, end int, bikeCost int) int {
-	if end == start {
-		if amounts[start] >= bikeCost {
-			return start
-		}
-
+func findDay(amounts []string, start int, end int, bikeCost int) int {
+	aEnd, _ := strconv.Atoi(amounts[end])
+	if aEnd < bikeCost {
 		return -1
+	}
+
+	if end == start {
+		return start
 	}
 
 	if end-start == 1 {
-		if amounts[start] >= bikeCost {
+		aStart, _ := strconv.Atoi(amounts[start])
+		if aStart >= bikeCost {
 			return start
 		}
 
-		if amounts[start+1] >= bikeCost {
-			return start + 1
-		}
-
-		return -1
+		return end
 	}
 
 	if end-start == 2 {
-		if amounts[start] >= bikeCost {
+		aStart, _ := strconv.Atoi(amounts[start])
+		if aStart >= bikeCost {
 			return start
 		}
 
-		if amounts[start+1] >= bikeCost {
+		aStartOne, _ := strconv.Atoi(amounts[start+1])
+		if aStartOne >= bikeCost {
 			return start + 1
 		}
 
-		if amounts[end] >= bikeCost {
-			return end
-		}
-
-		return -1
+		return end
 	}
 
-	var endNew int
-	var startNew int
-
-	if amounts[end/2] >= bikeCost {
-		endNew = end / 2
-		startNew = start
+	aEndHalf, _ := strconv.Atoi(amounts[end/2])
+	if aEndHalf >= bikeCost {
+		return findDay(amounts, start, end/2, bikeCost)
 	} else {
-		endNew = end
-		startNew = end/2 + 1
+		return findDay(amounts, end/2+1, end, bikeCost)
 	}
-
-	return findDay(amounts, startNew, endNew, bikeCost)
 }
 
 func openFile(path string) *os.File {
