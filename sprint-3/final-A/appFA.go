fee54a5e5603c8e370f63dbba8570e9ea9c8ebc9
18237432 <vpzorin@sberbank.ru>
2021-01-06 05:27:24 +0300

    appFA

--- sprint-3/final-A/appFA.go
+++ sprint-3/final-A/appFA.go
@@ -28,12 +28,22 @@ func Solve(reader *bufio.Reader, writer *bufio.Writer) {
 	writer.Flush()
 }
 
-func binarySearch(elements []string, st int, end int, k string) int {
-	if end == st+1 {
-		if elements[end] == k {
+type IntList struct {
+	str []string
+}
+
+func (list IntList) getEl(i int) (value int) {
+	value, _ = strconv.Atoi(list.str[i])
+
+	return
+}
+
+func binarySearch(elements IntList, st int, end int, k int) int {
+	if end-st <= 1 {
+		if elements.getEl(end) == k {
 			return end
 		}
-		if elements[st] == k {
+		if elements.getEl(st) == k {
 			return st
 		}
 
@@ -42,33 +52,48 @@ func binarySearch(elements []string, st int, end int, k string) int {
 
 	mid := (st + end) / 2
 
-	if elements[mid] == k {
+	if elements.getEl(mid) == k {
 		return mid
 	}
 
-	if elements[mid] > elements[end] {
-		if elements[mid] > k {
-			return binarySearch(elements, st, mid-1, k)
-		} else {
-			return binarySearch(elements, mid+1, end, k)
-		}
-	} else {
-		if elements[mid] < k {
-			return binarySearch(elements, mid+1, end, k)
-		} else {
-			return binarySearch(elements, st, mid-1, k)
-		}
+	onTail := elements.getEl(mid) > elements.getEl(end)
+	onHead := !onTail
+	kOnLeft := elements.getEl(mid) > k
+	kOnRight := !kOnLeft
+
+	if (onTail && kOnLeft) || (onHead && kOnLeft) {
+		return binarySearch(elements, st, mid-1, k)
 	}
+
+	if (onTail && kOnRight) || (onHead && kOnRight) {
+		return binarySearch(elements, mid+1, end, k)
+	}
+
+	//if elements[mid] > elements[end] {
+	//	if elements[mid] > k {
+	//		return binarySearch(elements, st, mid-1, k)
+	//	} else {
+	//		return binarySearch(elements, mid+1, end, k)
+	//	}
+	//} else {
+	//	if elements[mid] < k {
+	//		return binarySearch(elements, mid+1, end, k)
+	//	} else {
+	//		return binarySearch(elements, st, mid-1, k)
+	//	}
+	//}
+
+	return -1
 }
 
-func readData(reader *bufio.Reader) (n int, k string, elements []string) {
+func readData(reader *bufio.Reader) (n int, k int, elements IntList) {
 	line1, _ := reader.ReadString('\n')
 	line2, _ := reader.ReadString('\n')
 	line3, _ := reader.ReadString('\n')
 
 	n, _ = strconv.Atoi(strings.TrimRight(line1, "\n"))
-	k = strings.TrimRight(line2, "\n")
-	elements = strings.Fields(line3)
+	k, _ = strconv.Atoi(strings.TrimRight(line2, "\n"))
+	elements = IntList{strings.Fields(line3)}
 
 	return
 }
