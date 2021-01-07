c35d52782c21430d3227d06bee843079870d2630
18237432 <vpzorin@sberbank.ru>
2021-01-07 04:10:12 +0300

    appFB

--- sprint-3/final-B/appFB.go
+++ sprint-3/final-B/appFB.go
@@ -14,8 +14,10 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -28,41 +30,23 @@ func main() {
 	Solve(reader, writer)
 }
 
-type Interface interface {
-	Less(i, j int) bool
-	Swap(i, j int)
-	Len() int
+type Entry struct {
+	name string
+	prob int
+	fine int
 }
 
 type Leaderboard struct {
-	data []int
+	data []Entry
 }
 
 func Solve(reader *bufio.Reader, writer *bufio.Writer) {
-	arr := []int{0, 0, 1, 0, 0, 1}
-	var lb Interface = Leaderboard{arr}
-	//lb =
-	quickSort(lb, 0, 5)
-
-	fmt.Println(arr)
-	//writer.WriteString("\n")
-
-	//writer.Flush()
+	lb := readData(reader)
+	lb.Sort()
+	printLeaderBoard(lb, writer)
 }
 
-func (lb Leaderboard) Less(i, j int) bool {
-	return lb.data[i] < lb.data[j]
-}
-
-func (lb Leaderboard) Swap(i int, j int) {
-	lb.data[i], lb.data[j] = lb.data[j], lb.data[i]
-}
-
-func (lb Leaderboard) Len() int {
-	return len(lb.data)
-}
-
-func quickSort(data Interface, lo int, hi int) {
+func quickSort(data sort.Interface, lo int, hi int) {
 	if hi-lo <= 1 {
 		if data.Less(hi, lo) {
 			data.Swap(lo, hi)
@@ -91,6 +75,62 @@ func quickSort(data Interface, lo int, hi int) {
 
 	quickSort(data, lo, m)
 	quickSort(data, m, hi)
+}
+
+func (lb *Leaderboard) Sort() {
+	quickSort(lb, 0, lb.Len()-1)
+}
+
+func (lb *Leaderboard) Less(i, j int) bool {
+	a := lb.data[i]
+	b := lb.data[j]
+
+	if a.prob != b.prob {
+		return a.prob > b.prob
+	}
+
+	if a.fine != b.fine {
+		return a.fine < b.fine
+	}
+
+	return a.name < b.name
+}
+
+func (lb *Leaderboard) Swap(i int, j int) {
+	lb.data[i], lb.data[j] = lb.data[j], lb.data[i]
+}
+
+func (lb *Leaderboard) Len() int {
+	return len(lb.data)
+}
+
+func printLeaderBoard(lb *Leaderboard, writer *bufio.Writer) {
+	for _, entry := range lb.data {
+		writer.WriteString(entry.name)
+		writer.WriteString("\n")
+	}
+
+	writer.Flush()
+}
+
+func readData(reader *bufio.Reader) (lb *Leaderboard) {
+	sc := bufio.NewScanner(reader)
+	sc.Split(bufio.ScanLines)
+	sc.Scan()
+
+	lb = &Leaderboard{}
+
+	for sc.Scan() {
+		fields := strings.Fields(sc.Text())
+
+		name := fields[0]
+		prob, _ := strconv.Atoi(fields[1])
+		fine, _ := strconv.Atoi(fields[2])
+
+		lb.data = append(lb.data, Entry{name, prob, fine})
+	}
+
+	return
 }
 
 func openFile(path string) *os.File {
--- sprint-3/final-B/appFB_test.go
+++ sprint-3/final-B/appFB_test.go
@@ -25,25 +25,38 @@ import (
 //	}
 //}
 
+type Sortable []int
+
 type Case struct {
-	input    []int
-	expected []int
+	input    Sortable
+	expected Sortable
+}
+
+func (c Sortable) Less(i, j int) bool {
+	return c[i] < c[j]
+}
+
+func (c Sortable) Swap(i int, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c Sortable) Len() int {
+	return len(c)
 }
 
 func TestQuickSort(t *testing.T) {
 	cases := []Case{
-		{input: []int{3, 2, 1}, expected: []int{1, 2, 3}},
-		{input: []int{4, 3, 2, 1}, expected: []int{1, 2, 3, 4}},
-		{input: []int{4, 3, 3, 1}, expected: []int{1, 3, 3, 4}},
-		{input: []int{3, 2, 4, 1}, expected: []int{1, 2, 3, 4}},
-		{input: []int{3, 5, 2, 4, 1}, expected: []int{1, 2, 3, 4, 5}},
-		{input: []int{0, 0, 1, 0, 0, 1}, expected: []int{0, 0, 0, 0, 1, 1}},
+		{input: Sortable{3, 2, 1}, expected: Sortable{1, 2, 3}},
+		{input: Sortable{4, 3, 2, 1}, expected: Sortable{1, 2, 3, 4}},
+		{input: Sortable{4, 3, 3, 1}, expected: Sortable{1, 3, 3, 4}},
+		{input: Sortable{3, 2, 4, 1}, expected: Sortable{1, 2, 3, 4}},
+		{input: Sortable{3, 5, 2, 4, 1}, expected: Sortable{1, 2, 3, 4, 5}},
+		{input: Sortable{0, 0, 1, 0, 0, 1}, expected: Sortable{0, 0, 0, 0, 1, 1}},
 	}
 
 	for _, v := range cases {
-		cp := append([]int{}, v.input...)
-		var lb Interface = Leaderboard{cp}
-		quickSort(lb, 0, lb.Len()-1)
+		cp := append(Sortable{}, v.input...)
+		quickSort(cp, 0, cp.Len()-1)
 
 		if !reflect.DeepEqual(v.expected, cp) {
 			t.Errorf("\ncase:\n%v\n got: %v\nwant: %v", v.input, cp, v.expected)
