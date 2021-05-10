43d1ec11de7484d71e5729243f42b21d83ed03c9
Vasiliy Zorin <vpzorin@avito.ru>
2021-05-10 19:28:26 +0300

    s6 final B

--- sprint-5/finalB/main.go
+++ sprint-5/finalB/main.go
@@ -3,8 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	node := &Node{1, &Node{2, nil, nil}, &Node{3, nil, nil}}
+	node := &Node{1, &Node{2, nil, nil}, &Node{4, nil, nil}}
+	remove(node, 2)
 	fmt.Print(node, "\n")
+
+	node = &Node{5,
+		&Node{3,
+			&Node{2, nil, nil},
+			&Node{4, nil, nil},
+		},
+		&Node{6, nil, &Node{7, nil, nil}},
+	}
+
 	remove(node, 3)
-	fmt.Print(node)
+	fmt.Print(node, "\n")
 }
